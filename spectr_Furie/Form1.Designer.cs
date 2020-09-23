75913aecd5ba0d46b3a058dc60a0f411e79110c3
Ljudmila <nesmachnaja-ljudmila@rambler.ru>
2020-09-23 23:38:02 +0300

    First commit

---
+++ spectr_Furie/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace spectr_Furie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.спектрToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.спектрToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(301, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // спектрToolStripMenuItem
+            // 
+            this.спектрToolStripMenuItem.Name = "спектрToolStripMenuItem";
+            this.спектрToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.спектрToolStripMenuItem.Text = "Спектр";
+            this.спектрToolStripMenuItem.Click += new System.EventHandler(this.спектрToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 326);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem спектрToolStripMenuItem;
+    }
+}
+
---
+++ spectr_Furie/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace spectr_Furie
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // открытие изображения
+            Bitmap image;
+            OpenFileDialog open_dialog = new OpenFileDialog();
+            open_dialog.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*"; //формат загружаемого файла
+            if (open_dialog.ShowDialog() == DialogResult.OK) //если в окне была нажата кнопка "ОК"
+            {
+                try
+                {
+                    image = new Bitmap(open_dialog.FileName);
+                    pictureBox1.Size = image.Size;
+                    pictureBox1.Image = image;
+                    pictureBox1.Invalidate();
+                }
+                catch
+                {
+                    DialogResult rezult = MessageBox.Show("Невозможно открыть выбранный файл",
+                    "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            
+        }
+
+        private void спектрToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image==null)
+                {
+                    MessageBox.Show("Откройте изображение!");
+                    return;
+                }
+            int N = pictureBox1.Height; // n строк изображения, m столбцов
+            int M = N;
+            int j, k, i;
+            Bitmap image = new Bitmap(pictureBox1.Image);
+            var fxy = new float[image.Height, image.Width];     //массив предварительной обработки
+            var fuv = new float[image.Height, image.Width];     //массив преобразования фурье
+            var fuv_re = new float[image.Height, image.Width];  //массив действительной части
+            var fuv_im = new float[image.Height, image.Width];  //массив мнимой части
+            var fxv_re = new float[image.Height, image.Width];
+            var fxv_im = new float[image.Height, image.Width];
+
+            //-----------забираем пиксели в массив------------------
+            //-----------умножаем изображение на -1 в степени x+y---
+            for (i=0; i<image.Height; i++)  //высота
+                for (j=0; j<image.Width; j++)   //ширина
+                {
+                    // fxy[i, j] = Convert.ToInt32(image.GetPixel(j, i));     //забираем пиксели в массив
+                    Color col = image.GetPixel(j, i);
+                    //  int R = col.R;
+                    // int G = col.G;
+                    //  int B = col.B;
+                    //  fxy[i, j] = R * 1000000 + G * 1000 + B;
+                    fxy[i, j] = col.ToArgb();
+                    fxy[i, j] = fxy[i, j] * (float)Math.Pow(-1,i+j); //    умножаем функцию на -1 в степени 3
+                }
+            //MessageBox.Show(fxy[100, 100].ToString());
+
+            //------------первое число массива-строка, второе-столбец
+            //------------по строкам
+
+            float Wi = image.Width;
+            float Hi = image.Height;
+            for (i = 0; i < Hi; i++)  //высота
+                for (j = 0; j < Wi; j++)   //ширина
+                    for (k = 0; k < Wi; k++)
+                    {
+                        fxv_re[i, j] = fxv_re[i, j] + (float)Math.Round(fxy[i, k] * Math.Cos((2 * Math.PI * j * k) / Wi));
+                        fxv_im[i, j] = fxv_im[i, j] + (float)Math.Round(fxy[i, k] * Math.Sin((2 * Math.PI * j * k) / Wi));
+                    }
+            //MessageBox.Show(fxv_re[100, 100].ToString()+" & "+ fxv_im[100, 100].ToString());
+
+            //------------по столбцам
+
+            for (j = 0; j < Wi; j++)
+                for (i = 0; i < Hi; i++)
+                    for (k = 0; k < Hi; k++)
+                    {
+                        fuv_re[i, j] = fuv_re[i, j] + fxv_re[k, j] * (float)Math.Cos((2 * Math.PI * i * k) / Hi) + fxv_im[k, j] * (float)Math.Sin((2 * Math.PI * j * k) / Hi);
+                        fuv_im[i, j] = fuv_im[i, j] + fxv_re[k, j] * (float)Math.Sin((2 * Math.PI * i * k) / Hi) - fxv_im[k, j] * (float)Math.Cos((2 * Math.PI * j * k) / Hi);
+                    }
+
+            //------------выделяем действительную часть
+            for (i = 0; i < Hi; i++)
+                for (j = 0; j < Wi; j++)
+                {
+                    fuv[i, j] = (float)Math.Sqrt(Math.Pow(fuv_re[i, j],2) + Math.Pow(fuv_im[i, j],2));
+                }
+            //MessageBox.Show(fuv[100, 100].ToString());
+
+            //------------вывод спектра изображения
+            for (i = 0; i < Hi; i++)  
+                for (j = 0; j < Wi; j++)
+                {
+                    image.SetPixel(j, i, Color.FromArgb((int)Math.Round(fuv[i, j]))) ;   //выводим результат
+                   // image.SetPixel(j, i, Color.Beige);
+                    pictureBox1.Image = image;
+                }
+            //MessageBox.Show(fuv[100, 100].ToString());
+
+        }
+    }
+}
